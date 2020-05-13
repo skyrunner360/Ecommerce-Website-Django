e7dd813c6b64524d433e14d464a7b6e14d41e660
skyrunner360 <mathurrishi31@gmail.com>
2020-05-14 05:24:30 +0530

    made cart

--- ecw/shop/views.py
+++ ecw/shop/views.py
@@ -4,12 +4,20 @@ from .models import Product
 from math import ceil
 # Create your views here.
 def index(request):
-    products = Product.objects.all()
-    n= len(products)
-    nSlides = n//4 + ceil((n/4) - (n//4))
+    # products = Product.objects.all()
+    # n= len(products)
+    # nSlides = n//4 + ceil((n/4) - (n//4))
     # params = {'no_of_slides': nSlides,'range': range(1,nSlides),'product': products}
-    allprods = [[products,range(1, nSlides), nSlides],
-                [products,range(1, nSlides), nSlides]]
+    # allprods = [[products,range(1, nSlides), nSlides],
+    #             [products,range(1, nSlides), nSlides]]
+    allprods = []
+    catprods = Product.objects.values('category','id')
+    cats = {item ['category'] for item in catprods}
+    for cat in cats:
+         prod = Product.objects.filter(category=cat)
+         n = len(prod)
+         nSlides = n // 4 + ceil((n / 4) - (n // 4))
+         allprods.append([prod, range(1,nSlides),nSlides])
     params = {'allProds' : allprods}
     return render(request,'shop/index.html', params)
 def about(request):
