5df8041280684f62c6abdeda5330c6c09d0548ac
skyrunner360 <mathurrishi31@gmail.com>
2020-05-14 03:26:44 +0530

    updated sliders

--- ecw/shop/views.py
+++ ecw/shop/views.py
@@ -7,7 +7,10 @@ def index(request):
     products = Product.objects.all()
     n= len(products)
     nSlides = n//4 + ceil((n/4) - (n//4))
-    params = {'no_of_slides': nSlides,'range': range(1,nSlides),'product': products}
+    # params = {'no_of_slides': nSlides,'range': range(1,nSlides),'product': products}
+    allprods = [[products,range(1, nSlides), nSlides],
+                [products,range(1, nSlides), nSlides]]
+    params = {'allProds' : allprods}
     return render(request,'shop/index.html', params)
 def about(request):
     return render(request, 'shop/about.html')
